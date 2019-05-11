965a9220e8f6e3a57b831bfa2d9140adc9aca8b5
jgi <public-vs2015@gissehel.org>
2019-05-11 18:12:26 +0200

    FEATURE : Allow to link directly a name using window.location.hash

--- index.js
+++ index.js
@@ -4,9 +4,32 @@ readyPromise.then(() => {
     const avatar = document.getElementById('avatar');
     const form = document.getElementById('form');
 
-    const onChange = () => {
-        let name = nameElement.value || '';
+    const getHash = () => {
+        let hash = window.location.hash;
+        if (hash.length>0 && hash.startsWith('#')) {
+            hash = hash.slice(1);
+        }
+        return decodeURI(hash);
+    };
+    const setHash = (name) => {
+        let hash = getHash();
+        if (hash !== name) {
+            window.location.hash = name;
+        }
+    };
+    const getInput = () => nameElement.value;
+    const setInput = (name) => {
+        let input = getInput();
+        if (input !== name) {
+            nameElement.value = name;
+        }
+    }
+
+    const generateNewAvatar = (name) => {
+        name = name || '';
         name = name.trim();
+        setInput(name);
+        setHash(name);
         if (name === '') {
             name = md5(`${Math.random()}`).slice(0, 8)
         }
@@ -16,8 +39,17 @@ readyPromise.then(() => {
             avatar.innerHTML = '';
         }
         nameShown.textContent = `~ ${name} ~`;
+    };
+    const onChange = () => {
+        const name = getInput();
+        generateNewAvatar(name);
     }
-    onChange();
+    const onHashChange = () => {
+        const name = getHash();
+        generateNewAvatar(name);
+    }
+    onHashChange();
+    window.addEventListener('hashchange', (e) => onHashChange());
     nameElement.addEventListener('input', (e) => onChange());
     form.addEventListener('submit', (e) => {
         e.preventDefault();
